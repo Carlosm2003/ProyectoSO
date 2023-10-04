20fa1b6746eca9ddd16436f44ff7b9723118003d
Carlos Muñoz Gil <carlos.munoz.gil@estudiantat.upc.edu>
2023-10-04 14:14:55 +0200

    Primera Version

---
+++ databasearchivo.c
@@ -0,0 +1,65 @@
+//programa en C para consultar los datos de la base de datos
+//Incluir esta libreria para poder hacer las llamadas en shiva2.upc.es
+//#include <my_global.h>
+#include <mysql.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	MYSQL *conn;
+	int err;
+	// Estructura especial para almacenar resultados de consultas 
+	MYSQL_RES *resultado;
+	MYSQL_ROW row;
+	//Creamos una conexion al servidor MYSQL 
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexiￃﾳn: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//inicializar la conexion
+	conn = mysql_real_connect (conn, "localhost","user", "pass", "personas",0, NULL, 0);
+	if (conn==NULL) {
+		printf ("Error al inicializar la conexiￃﾳn: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	// consulta SQL para obtener una tabla con todos los datos
+	// de la base de datos
+	err=mysql_query (conn, "SELECT Jugador.Usuario, Logros.descripcion FROM (Jugador, Logros, Jug_Log) WHERE Jug_Log.logro_obtenido = 'yes' ");
+	if (err!=0) {
+		printf ("Error al consultar datos de la base %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//recogemos el resultado de la consulta. El resultado de la
+	//consulta se devuelve en una variable del tipo puntero a
+	//MYSQL_RES tal y como hemos declarado anteriormente.
+	//Se trata de una tabla virtual en memoria que es la copia
+	//de la tabla real en disco.
+	resultado = mysql_store_result (conn);
+	// El resultado es una estructura matricial en memoria
+	// en la que cada fila contiene los datos de una persona.
+	
+	// Ahora obtenemos la primera fila que se almacena en una
+	// variable de tipo MYSQL_ROW
+	row = mysql_fetch_row (resultado);
+	// En una fila hay tantas columnas como datos tiene una
+	// persona. En nuestro caso hay tres columnas: dni(row[0]),
+	// nombre(row[1]) y edad (row[2]).
+	if (row == NULL)
+		printf ("No se han obtenido datos en la consulta\n");
+	else
+		while (row !=NULL) {
+
+			printf ("El Jugador %s, ha obtenido el logro: %s\n", (row[0]), (row[1]));
+			// obtenemos la siguiente fila
+			row = mysql_fetch_row (resultado);
+		}
+	}
+	// cerrar la conexion con el servidor MYSQL 
+	mysql_close (conn);
+	exit(0);
+}
